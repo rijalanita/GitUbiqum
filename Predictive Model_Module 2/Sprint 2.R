d5c5af9a343d18f2cc2ece0a5d540586f84c1971
rijalanita <anitarijal@gmail.com>
2020-02-11 15:27:54 +0100

    module 2 update

--- Predictive Model_Module 2/Sprint 2.R
+++ Predictive Model_Module 2/Sprint 2.R
@@ -4,8 +4,44 @@
 library(readr)
 library(ggplot2)
 library(dplyr)
-library(caret)
-install.packages("lattice")
 library(lattice)
-
+library(caret)
+8
+#Module 2
+#Predicting Brand Preference
 response <- read_csv("Data/CompleteResponses.csv")
+summary(response)
+
+#convert [zipcode, car, elevel] into factors
+response <- response %>% 
+  mutate_at(c("elevel", "zipcode", "car"), factor)
+
+#scaling [columns salary, age] 
+
+#?preProcess
+numerics <- c("salary" , "age" , "credit")
+ppvalues <- preProcess(response[, numerics], 
+                       method = c("center", "scale"),
+                       verbose = FALSE) 
+
+ppvalues
+
+#splitting the data
+set.seed(108)
+inTrain <- createDataPartition(response$brand, 
+                               times = 1,
+                               p = 0.7,
+                               list = FALSE)
+
+#?createDataPartition
+
+responseTrain <- response[inTrain, ]
+responseTest <- response[-inTrain, ]
+
+#?trainControl
+
+# repeat 
+trainmodel <- trainControl(method = "cv",
+                           number = 10,
+                           search = grid)
+
