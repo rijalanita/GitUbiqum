dfa8f77147dcc738e090f5e39cf556332e7e68d4
rijalanita <anitarijal@gmail.com>
2020-02-13 10:59:20 +0100

    Ran randomforest and c5model
    Received prediction results
    
    Assessed accuracy of model

--- Predictive Model_Module 2/Sprint 2.R
+++ Predictive Model_Module 2/Sprint 2.R
@@ -6,39 +6,56 @@ library(ggplot2)
 library(dplyr)
 library(lattice)
 library(caret)
+library(tidyr)
 
 #Module 2
-#Predicting Brand Preference
-response <- read_csv("Data/CompleteResponses.csv")
-summary(response)
+
+response<-read.csv2("Data/surveyBelkinElago.csv")
+response
+
+incomplete <- subset(response, brand == " ")
+
+complete <- subset(response, brand !=" ")
+
 
 #convert [zipcode, car, elevel] into factors
-response <- response %>% 
+complete <- complete %>% 
   mutate_at(c("elevel", "zipcode", "car", "brand"), factor)
 
-#scaling [columns salary, age] 
-
-#?preProcess
-numerics <- c("salary" , "age" , "credit")
-ppvalues <- preProcess(response[, numerics], 
-                       method = c("center", "scale"),
-                       verbose = FALSE) 
-?predict
-
-ppvalues()
 
 #splitting the data
 set.seed(108)
-inTrain <- createDataPartition(response$brand, 
+inTrain <- createDataPartition(complete$brand, 
                                times = 1,
                                p = 0.7,
                                list = FALSE)
 
 #?createDataPartition
 
-responseTrain <- response[inTrain, ]
-responseTest <- response[-inTrain, ]
+completeTrain <- complete[inTrain, ]
+completeTest <- complete[-inTrain, ]
 
+#scaling [columns salary, age] 
+
+#?preProcess
+#choosing features salary and age as predictors
+
+numerics <- c("salary" , "age")
+
+#run the preprocess function on  salary and age features to scale the data
+ppvalues <- preProcess(complete[, numerics], 
+                       method = c("center", "scale"),
+                       verbose = FALSE) 
+
+head(ppvalues)
+#NOTE: preProcess estimates mean and sd of the preprocess values but doesn't actually do the transformation
+
+#Use the preprocess method to scale, center the training and testing data sets
+proctrain <- predict(ppvalues,completeTrain)
+proctest <- predict(ppvalues, completeTest)
+incomplete <- predict(ppvalues, incomplete)
+incomplete
+View(proctrain)
 ?trainControl
 
 #Building the model
@@ -46,16 +63,39 @@ responseTest <- response[-inTrain, ]
 #Defining how we would like the model  built
 fitting <- trainControl(method = "cv",
                         number = 10,
-                        search = "grid")
-
-#RF Model
+                        search = "grid",
+                        classProbs = TRUE,
+                        summaryFunction = twoClassSummary,
+                        savePredictions = "final")
+str(proctrain)
+#Random Forest Model
 #?train
 
-RF <- train(brand~., 
-            data = responseTrain,
+set.seed(108)
+RFmodel <- train(brand~salary+age, 
+            data = proctrain,
             method = "ranger",
             trControl = fitting,
-            tuneLength = 2)
+            tuneLength = 2, metric = "ROC")
+RFmodel
+summary(RFmodel)
 
-RF
-library(randomForest)
+#?predict
+rfresults <- predict(RFmodel, incomplete)
+rfresults
+
+postResample(rfresults, proctest$brand)
+confusionMatrix(rfresults, proctest$brand)
+
+set.seed(108)
+C5Model <- train(x = proctrain[,numerics],
+                 y = proctrain$brand,
+                 method = "C5.0",
+                 trControl = fitting,
+                 tunelength = 2)
+
+c5results <- predict(C5Model, incomplete)
+
+?postResample
+postResample(c5results, proctest$brand)
+
