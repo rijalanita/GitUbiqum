b02e3ccdf5a6502dba2943e91e8fc1d2b7eb8dc7
rijalanita <anitarijal@gmail.com>
2020-02-14 13:33:02 +0100

    Commit Sprint 2 files

--- Predictive Model_Module 2/Script/Sprint 2.R
+++ Predictive Model_Module 2/Script/Sprint 2.R
@@ -130,4 +130,99 @@ names(total)
 summary(total)
 
 
+<<<<<<< HEAD
 write.csv(total, "FinalCSV")
+=======
+#Module 2
+
+response<-read.csv2("Data/surveyBelkinElago.csv")
+response
+
+incomplete <- subset(response, brand == " ")
+
+complete <- subset(response, brand !=" ")
+
+
+#convert [zipcode, car, elevel] into factors
+complete <- complete %>% 
+  mutate_at(c("elevel", "zipcode", "car", "brand"), factor)
+
+
+#splitting the data
+set.seed(108)
+inTrain <- createDataPartition(complete$brand, 
+                               times = 1,
+                               p = 0.7,
+                               list = FALSE)
+
+#?createDataPartition
+
+completeTrain <- complete[inTrain, ]
+completeTest <- complete[-inTrain, ]
+
+#scaling [columns salary, age] 
+
+#?preProcess
+#choosing features salary and age as predictors
+
+numerics <- c("salary" , "age")
+
+#run the preprocess function on  salary and age features to scale the data
+ppvalues <- preProcess(complete[, numerics], 
+                       method = c("center", "scale"),
+                       verbose = FALSE) 
+
+head(ppvalues)
+#NOTE: preProcess estimates mean and sd of the preprocess values but doesn't actually do the transformation
+
+#Use the preprocess method to scale, center the training and testing data sets
+proctrain <- predict(ppvalues,completeTrain)
+proctest <- predict(ppvalues, completeTest)
+
+incomplete <- predict(ppvalues, incomplete)
+incomplete
+View(proctrain)
+?trainControl
+
+#Building the model
+
+#Defining how we would like the model  built
+fitting <- trainControl(method = "cv",
+                        number = 10,
+                        search = "grid",
+                        classProbs = TRUE,
+                        summaryFunction = twoClassSummary,
+                        savePredictions = "final")
+str(proctrain)
+#Random Forest Model
+#?train
+
+set.seed(108)
+RFmodel <- train(brand~salary+age, 
+            data = proctrain,
+            method = "ranger",
+            trControl = fitting,
+            tuneLength = 2, metric = "ROC")
+RFmodel
+summary(RFmodel)
+
+#?predict
+rfresults <- predict(RFmodel, incomplete)
+rfresults
+
+postResample(rfresults, proctest$brand)
+confusionMatrix(rfresults, proctest$brand)
+
+set.seed(108)
+C5Model <- train(x = proctrain[,numerics],
+                 y = proctrain$brand,
+                 method = "C5.0",
+                 trControl = fitting,
+                 tunelength = 2)
+
+c5results <- predict(C5Model, incomplete)
+
+?postResample
+postResample(c5results, proctest$brand)
+
+>>>>>>> 15e30799c6f6d2e715adcd6cc70606cac0ed5b0c
